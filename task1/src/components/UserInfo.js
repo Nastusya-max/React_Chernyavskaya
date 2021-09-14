4a9b18ad129ea425a40320b2f24e0a426dfb16f7
Nastusya-max <Nastusya-max>
2021-09-14 13:47:27 +0300

    fixed

--- task1/src/components/UserInfo.js
+++ task1/src/components/UserInfo.js
@@ -4,7 +4,6 @@ import UserDetails from './UserDetails'
 function UserInfo({ user, avatar }) {
   return <div className="user__details">
     <Avatar avatar={avatar} />
-    {/* <UserDetails name={user.name} username={user.username} email={user.email} companyName={user.company.name} /> */}
     <UserDetails  user={user}/>
   </div>
 }
