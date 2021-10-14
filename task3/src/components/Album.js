7ab7c4a7e721ec55a38db86ed571d45f0fa6ed88
Nastusya-max <Nastusya-max>
2021-10-14 12:42:14 +0300

    from class to hook

--- task3/src/components/Album.js
+++ task3/src/components/Album.js
@@ -1,7 +1,10 @@
+import { useCallback } from "react";
+
 function Album({ album }) {
   const { title } = album;
+  const albumClickHandler = useCallback((album) => {console.log(album) }, []);
 
-  return <li>{title}</li>
+  return <li onClick={() => albumClickHandler(album)}>{title}</li>
 }
 
 export default Album;
--- task3/src/components/Content.js
+++ task3/src/components/Content.js
@@ -1,22 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Album from "./Album";
 
-class Content extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { albums: [] };
-  }
+function Content() {
+  const [albums, setAlbums] = useState([]);
 
-  componentDidMount = () => {
-    fetch('https://jsonplaceholder.typicode.com/albums').then((res) => {
-      return res.json();
-    }).then(data => this.setState({ albums: data }));
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch('https://jsonplaceholder.typicode.com/albums').then((res) => {
+        return res.json();
+      }).then(data => setAlbums(data));
+    };
+    fetchData();
+  }, []);
 
-  render() {
-    return <div className="user__details">
-     <ul className='content__component text'>{this.state.albums.map(album => <Album album={album}  key={album.id}/>)}</ul> </div>
-  }
+  return <div className="user__details">
+    <ul className='content__component text'>{albums.map(album => <Album album={album} key={album.id} />)}</ul>
+  </div>
 }
 
 export default Content;
