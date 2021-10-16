c577b94172f95e2e479d9f9eaa631467deb27b65
Nastusya-max <Nastusya-max>
2021-10-16 14:01:23 +0300

    fix button

--- task3/src/components/Album.js
+++ task3/src/components/Album.js
@@ -5,44 +5,32 @@ import Photos from "./Photos";
 function Album({ album }) {
   const { title } = album;
   const [activeAlbum, setActiveAlbum] = useState(null);
-  let [photos, setPhotos] = useState([]);
-  const [closedAlbum, setClosedAlbum] = useState(null);
+  const [photos, setPhotos] = useState([]);
+  const [isOpenAlbum, setIsOpenAlbum] = useState(true);
 
-  const albumClickHandler = useCallback((album) => { setActiveAlbum(album);}, []);
-  const buttonClickHandler =  useCallback((album) => { setClosedAlbum(album)}, []);
+  const albumClickHandler = useCallback((album) => { setActiveAlbum(album); }, []);
+  const closeAlbum = useCallback(() => { setPhotos([]); }, []);
 
   useEffect(() => {
-    // console.log('activeAlbum');
     if (activeAlbum) {
-      console.log(activeAlbum);
       const fetchData = async () => {
         fetch(`https://jsonplaceholder.typicode.com/albums/${activeAlbum.userId}/photos`).then((res) => {
           return res.json();
-        }).then(photos => {
-          setPhotos(photos);
-          // console.log(photos)
-        });
+        }).then(photos => setPhotos(photos));
       };
       fetchData();
     }
-  }, [activeAlbum])
-
-  useEffect(() => {
-    if (closedAlbum) {
-      console.log(closedAlbum);
-      setPhotos([]);
-    }
-  }, [closedAlbum])
+  }, [activeAlbum, isOpenAlbum])
 
   return <div className='content-container'>
     {photos.length ?
-      <div>
-        <div onClick={() => buttonClickHandler(album)}><Button /></div>
+      <div className='album-container'>
+        <Button album={album} closeAlbum={closeAlbum} />
         <div className='img-container'>
           {photos.map(photo => <Photos photo={photo} key={photo.id} />)}
         </div>
       </div>
-      : <li onClick={() => {albumClickHandler(album); }}>{title}</li>}
+      : <li onClick={() => { setIsOpenAlbum(!isOpenAlbum); albumClickHandler(album) }} >{title}</li>}
   </div>
 }
 
