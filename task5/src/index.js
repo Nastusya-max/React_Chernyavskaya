a36d74269b0c0d0d8805900b0cd8535eb597baae
Nastusya-max <Nastusya-max>
2021-11-02 17:58:23 +0300

    ErrorBoundry

--- task5/src/index.js
+++ task5/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
 import store from './store/store';
+import ErrorBoundary from './errors/ErrorBoundary'
 
 ReactDOM.render(
   <React.StrictMode>
+    <ErrorBoundary>
     <Provider store={store}>
       <App />
-    </Provider>,
+    </Provider>
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );
